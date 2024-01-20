5ab685da2a6bd22bd692945d6f977b8a549316c2
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-01-21 10:05:24 +1100

    findOne, findAll and delete

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -45,4 +45,18 @@ export class Product {
     type: 'text',
   })
   gender: string;
+
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+
+  // @BeforeUpdate(){}
 }
