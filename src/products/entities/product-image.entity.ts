83bddc1ee51788cee713436c99f17013d1d1c6a0
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-01-22 09:08:07 +1100

    Create images entity and relation with product entity

---
+++ src/products/entities/product-image.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Product } from './product.entity';
+
+@Entity()
+export class ProductImage {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  url: string;
+
+  @ManyToOne(() => Product, (product) => product.images) //Muchas imagenes pueden tener un solo producto
+  product: Product;
+}
