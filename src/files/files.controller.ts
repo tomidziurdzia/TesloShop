bec8d103b2becdf4e6cdb699578955f926961f55
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-01-22 11:25:55 +1100

    Subir archivo a backend y validar" git push

---
+++ src/files/files.controller.ts
@@ -0,0 +1,30 @@
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    return {
+      fileName: file.originalname,
+    };
+  }
+}
